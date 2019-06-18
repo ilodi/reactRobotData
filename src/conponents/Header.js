cc302ba261ebabb4e385e9c1e8ff495c87c27246
David E <lodi.dev@outlook.com>
2019-06-18 12:46:11 -0500

    Add scss loader

--- src/conponents/Header.js
+++ src/conponents/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = ({ title }) => (
   <header>
-    <h1 className="text-center">{title}</h1>
+    <h1 className="text-center">{title} dd </h1>
   </header>
 );
 
